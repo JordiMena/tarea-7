67a8b4d192452eb3b9c9c55948ba7b32add1afe4
JordiMena <jjyordy78@gmail.com>
2024-07-30 23:11:08 -0400

    Add files via upload

---
+++ ControlDeInventario.Ioc/DependicyInjection.cs
@@ -0,0 +1,16 @@
+﻿using ControlDelInventario.Domain.Repository;
+using ControlDelInventario.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ControlDelInventario.IoC
+{
+    public static class DependencyInjection
+    {
+        public static void AddProjectDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<IProductoRepository, ProductoRepository>();
+            // Agrega más servicios según sea necesario
+        }
+    }
+}
+
